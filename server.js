152f5edca15ecfcbbfbbde404f35c1fe5e60eba9
Prathm98 <wakodikarp1998@gmail.com>
2020-07-05 18:13:11 +0530

    DB connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Connect Database
+connectDB();
 
 app.get("/", function (req, res) {
   res.send("Hello World");
