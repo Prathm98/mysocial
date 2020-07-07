635d731fc4eb4cd8c687711ebfb3ec0d5aef4a82
Prathm98 <wakodikarp1998@gmail.com>
2020-07-08 00:53:45 +0530

    User registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -21,12 +23,45 @@ router.post(
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
   ],
-  function (req, res) {
+  async function (req, res) {
     const errors = validationResult(req);
     if( !errors.isEmpty() ){
       return res.status(400).json({errors: errors.array()});
     }
-    res.send("Users route.");
+
+    const {name, email, password} = req.body;
+    try {
+      // See if user exist
+      let user = await User.findOne({email});
+      if(user){
+        return res.status(400).json({errors: ["User already exists."] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      // Encrypt password
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      user.save();
+
+      // Return jsonwebtoken
+      res.send("Users registered successfully.");
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Server Gone");
+    }
   }
 );
 
