e13a099144d2b72d34baa627f597970d3b1fb0e7
Prathm98 <wakodikarp1998@gmail.com>
2020-07-08 01:41:19 +0530

    JWT implementation and authorization

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -57,7 +59,21 @@ router.post(
       user.save();
 
       // Return jsonwebtoken
-      res.send("Users registered successfully.");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecrete'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+
     } catch (e) {
       console.log(e);
       res.status(500).send("Server Gone");
