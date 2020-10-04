cb8721064cc0b9bd4bacbb5e9c6f6e57ffa12a8c
Prathm98 <wakodikarp1998@gmail.com>
2020-10-04 20:27:42 +0530

    React Initial setup with Navbar and landing component

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
