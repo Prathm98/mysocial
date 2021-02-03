d4f024055195252e4de199e18dd0ad289982318c
Prathmesh Wakodikar <46840479+Prathm98@users.noreply.github.com>
2021-02-04 02:41:25 +0530

    Initial Setup
    Created Express server, added DB connection, define basic routes.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+
+// Connect DB
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("API running"));
+
+// Defining Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
